e3928cbfa4ece35e6636635c9d839537bb1076db
Javier <javierboudere@gmail.com>
2024-11-11 16:10:36 +0100

    .

--- src/Two_Sum/Solution.java
+++ src/Two_Sum/Solution.java
@@ -3,7 +3,7 @@ package Two_Sum;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Two_Sum {
+public class Solution {
 	public int[] twoSum(int[] nums, int target) {
 		Map<Integer, Integer> numMap = new HashMap<>();
 		int n = nums.length;
