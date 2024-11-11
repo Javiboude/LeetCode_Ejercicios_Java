631f939dc67eec1d873e97e1dd863de4d0cf4a4a
Javier <javierboudere@gmail.com>
2024-11-11 16:08:41 +0100

    Commit ejercicio Two Sum

---
+++ src/Two_Sum/Two_Sum.java
@@ -0,0 +1,24 @@
+package Two_Sum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Two_Sum {
+	public int[] twoSum(int[] nums, int target) {
+		Map<Integer, Integer> numMap = new HashMap<>();
+		int n = nums.length;
+
+		for (int i = 0; i < n; i++) {
+			numMap.put(nums[i], i);
+		}
+
+		for (int i = 0; i < n; i++) {
+			int complement = target - nums[i];
+			if (numMap.containsKey(complement) && numMap.get(complement) != i) {
+				return new int[] { i, numMap.get(complement) };
+			}
+		}
+
+		return new int[] {};
+	}
+}
